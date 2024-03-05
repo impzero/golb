813361348e162b69d94918ce949567da398c50b4
Lyubo Lyubchev <lyubo_2317@abv.bg>
2024-03-05 17:14:04 +0100

    refactor load balancer and health check implementation

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
+	"time"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/impzero/golb/backend"
 )
 
@@ -15,39 +19,82 @@ const (
 )
 
 type Service struct {
-	Hostname string
-	Healthy  bool
+	Name    string
+	URL     string
+	Healthy bool
 }
 
 type LoadBalancer struct {
-	Pool     map[string][]Service
+	Pool     []Service
 	Strategy Strategy
 }
 
-func NewLoadBalancer(strategy Strategy) *LoadBalancer {
+func NewLoadBalancer(strategy Strategy, pool []Service) *LoadBalancer {
 	return &LoadBalancer{
 		Strategy: strategy,
+		Pool:     pool,
 	}
 }
 
+func (lb *LoadBalancer) CheckHealth() {
+	for {
+		for i, serv := range lb.Pool {
+			i := i
+			serv := serv
+			go func() {
+				resp, err := http.Get(fmt.Sprintf("%s/health", serv.URL))
+				if err != nil {
+					lb.Pool[i].Healthy = false
+					return
+				}
+				lb.Pool[i].Healthy = resp.StatusCode == http.StatusOK
+			}()
+		}
+
+		spew.Dump(lb.HealthStatus())
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func (lb *LoadBalancer) HealthStatus() map[string]string {
+	status := map[string]string{}
+	for _, serv := range lb.Pool {
+		if serv.Healthy {
+			status[serv.Name] = "healthy"
+		} else {
+			status[serv.Name] = "unhealthy"
+		}
+	}
+	return status
+}
+
 func (lb *LoadBalancer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	proxy := httputil.NewSingleHostReverseProxy(r.URL)
+	targetURL, err := url.Parse("http://localhost:8080")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
 	proxy.ServeHTTP(w, r)
 }
 
 func main() {
-	be1 := backend.NewService("be1")
-	be2 := backend.NewService("be2")
+	pool := []Service{}
+	for i := 0; i < 10; i++ {
+		bs := backend.NewService(fmt.Sprintf("be-%d", i), fmt.Sprintf(":%d", 8001+i))
+		go func() {
+			log.Fatal(bs.Serve())
+		}()
 
-	go func() {
-		log.Fatal(be1.Serve(":8080"))
-	}()
+		pool = append(pool, Service{
+			Name:    bs.Name,
+			URL:     bs.URL(false),
+			Healthy: true,
+		})
+	}
 
-	go func() {
-		log.Fatal(be2.Serve(":8081"))
-	}()
+	lb := NewLoadBalancer(StrategyRoundRobin, pool)
 
-	lb := NewLoadBalancer(StrategyRoundRobin)
-
+	go lb.CheckHealth()
 	log.Fatal(http.ListenAndServe(":8000", lb))
 }
